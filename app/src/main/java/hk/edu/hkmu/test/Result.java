ebd64a62f36c660e7e009f87af491e4c00d4cc4e
FinnTheHuman411 <lbenson997@gmail.com>
2022-04-21 19:39:22 +0800

    update

--- app/src/main/java/hk/edu/hkmu/test/Result.java
+++ app/src/main/java/hk/edu/hkmu/test/Result.java
@@ -3,6 +3,7 @@ package hk.edu.hkmu.test;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
@@ -76,13 +77,33 @@ public class Result extends AppCompatActivity {
                         HashMap<String,String> contact = searching.searchlist.get(position);
                         AlertDialog.Builder builder = new AlertDialog.Builder(Result.this);
                         builder.setTitle(contact.get(SchoolInfo.enname));
-                        builder.setMessage(Html.fromHtml(
-                                "<b>Mobile: </b>" + contact.get(SchoolInfo.entel) + "<br/>" +
-                                        "<b>Location: </b>" + contact.get(SchoolInfo.enaddress)
-                        ));
 
-                        builder.setNeutralButton("Location",null);
-                        builder.setPositiveButton("Details",null);
+                        String locButton = "";
+                        String detailButton = "";
+
+                        if(Locale.getDefault().getLanguage().equals(new Locale("en").getLanguage())) {
+                            builder.setMessage(Html.fromHtml(
+                                    "<b>Mobile: </b>" + contact.get(SchoolInfo.entel) + "<br/>" +
+                                            "<b>Location: </b>" + contact.get(SchoolInfo.enaddress)
+                            ));
+                            locButton = "Location";
+                            detailButton = "Details";
+                        }else if(Locale.getDefault().getLanguage().equals(new Locale("zh").getLanguage())){
+                            builder.setMessage(Html.fromHtml(
+                                    "<b>聯絡電話: </b>" + contact.get(SchoolInfo.chtel) + "<br/>" +
+                                            "<b>地址: </b>" + contact.get(SchoolInfo.chaddress)
+                            ));
+                            locButton = "位置";
+                            detailButton = "詳細資料";
+                        }
+
+                        builder.setNeutralButton(locButton,null);
+                        builder.setPositiveButton(detailButton,new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                setContentView(R.layout.sku_details);
+                            }
+                        });
 
                         AlertDialog alertDialog = builder.create();
 
